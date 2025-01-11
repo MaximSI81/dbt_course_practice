cc0ec74f6b468d1ed4970ee8bb42ae5ab8ff04ad
Maxim Safonov <safonovmio@gmail.com>
2025-01-12 00:46:04 +0300

    commit_models_3

---
+++ models/intermediate/flight/fct_bookings.sql
@@ -0,0 +1,9 @@
+{{
+  config(
+    materialized = 'table',
+    )
+}}
+
+select "book_ref", "book_date", "total_amount"
+from 
+    {{ ref('stg_flight__bookings') }}
---
+++ models/intermediate/flight/fct_tickets.sql
@@ -0,0 +1,8 @@
+{{
+  config(
+    materialized = 'table',
+    )
+}}
+
+select ticket_no, book_ref, passenger_id, passenger_name, contact_data
+from {{ ref('stg_flight__tickets') }}
